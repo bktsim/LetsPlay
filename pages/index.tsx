256de39351bb5785864666561ea8c97fed5f7399
Rustam Nassyrov <70337011+rustamch@users.noreply.github.com>
2022-10-01 16:55:34 -0700

    Pushing merge conflict resolved.

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useTheme } from "@nextui-org/react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
-import ClubCreation from "./src/components/ClubCreation";
+import ClubCreation from "./src/components/EventCreation";
 import CustomNavbar from "./src/components/CustomNavbar";
 import EventCard from "./src/components/EventCard";
 import ProfileCard from "./src/components/ProfileCard";
