a4f0de70ae2d1c727ff148ff74ffcf525b3ef1e2
Rustam Nassyrov <70337011+rustamch@users.noreply.github.com>
2022-10-01 17:26:49 -0700

    Added server.

--- pages/api/server.ts
+++ pages/api/server.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 8080; // default port to listen
 
 // define a route handler for the default home page
-app.get("/", (req, res) => {
+app.post("/auth", (req, res) => {
     res.send("Hello world!");
 });
 
