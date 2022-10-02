dda9d942bd0201e9cf78792cace61300043fd8b6
bktsim <brendontsim@gmail.com>
2022-10-02 15:27:15 -0700

    minor fixes

--- pages/src/components/LoginModal.tsx
+++ pages/src/components/LoginModal.tsx
@@ -1,64 +1,59 @@
 /* eslint-disable react-hooks/rules-of-hooks */
 import {
-    Image,
-    Card,
-    Container,
-    Modal,
-    Text,
-    Input,
-    Badge,
-    Button,
-    Spacer,
-    Checkbox,
-    Row,
+  Image,
+  Card,
+  Container,
+  Modal,
+  Text,
+  Input,
+  Badge,
+  Button,
+  Spacer,
+  Checkbox,
+  Row,
 } from "@nextui-org/react";
 import { useContext, useState } from "react";
 import { LoginContext } from "../../_app";
 import useStorage from "../hooks/storage";
 
 export const LoginModal = () => {
-    const { loggedIn, setLoggedIn, user, setUser } = useContext(LoginContext)
-    let password = "";
-    let email = "";
-    return (
-        <div>
-            <Modal
-                preventClose
-                aria-labelledby="modal-title"
-                open={!loggedIn}
-
-                blur
-            >
-                <Modal.Header>
-                    <Text id="modal-title" size={18}>
-                        Welcome to
-                        <Text b size={18}>
-                            LetsPlay
-                        </Text>
-                    </Text>
-                </Modal.Header>
-                <Modal.Body>
-                    <Input
-                        clearable
-                        bordered
-                        fullWidth
-                        color="primary"
-                        size="lg"
-                        placeholder="Email"
-                        onChange={(e) => email = e.target.value}
-                    // contentLeft={<Mail fill="currentColor" />}
-                    />
-                    <Input
-                        clearable
-                        bordered
-                        fullWidth
-                        color="primary"
-                        size="lg"
-                        placeholder="Password"
-                        onChange={(e) => password = e.target.value}
-                    // contentLeft={<Password fill="currentColor" />}
-                    />
-                    {/* <Row justify="space-between">
+  const { loggedIn, setLoggedIn, user, setUser } = useContext(LoginContext);
+  let password = "";
+  let email = "";
+  return (
+    <div>
+      <Modal preventClose aria-labelledby="modal-title" open={!loggedIn} blur>
+        <Modal.Header>
+          <Text id="modal-title" size={18}>
+            Welcome to
+            <Text b size={18} css={{ marginLeft: 5 }}>
+              LetsPlay
+            </Text>
+          </Text>
+        </Modal.Header>
+        <Modal.Body>
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            placeholder="Email"
+            onChange={(e) => (email = e.target.value)}
+            // contentLeft={<Mail fill="currentColor" />}
+          />
+          <Input
+            clearable
+            bordered
+            fullWidth
+            color="primary"
+            size="lg"
+            type="password"
+            placeholder="Password"
+            onChange={(e) => (password = e.target.value)}
+            // contentLeft={<Password fill="currentColor" />}
+          />
+          {/* <Row justify="space-between">
                         <Checkbox
                             onClick={useStorage().setItem("loggedIn", "session", "true")}
                         >
@@ -66,35 +61,42 @@ export const LoginModal = () => {
                         </Checkbox>
                         <Text size={14}>Forgot password?</Text>
                     </Row> */}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button auto onClick={() => {
-                        fetch("/api/login", {
-                            method: "POST",
-                            headers: {
-                                "Content-type": "application/json; charset=UTF-8"
-                            },
-                            body: JSON.stringify({
-                                email: email,
-                                password: password
-                            })
-                        }).then(res => {
-                            if (res.status === 200) {
-                                setLoggedIn(true)
-                                return res.json().then(data => {
-                                    setUser(data);
-                                    useStorage().setItem("loggedIn", "true", "session");
-                                    useStorage().setItem("user", JSON.stringify(data), "session");
-                                })
-                            } else {
-                                res.json().then((data) => console.log(data));
-                            }
-                        })
-                    }}>
-                        Sign in
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </div>
-    )
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            auto
+            onClick={() => {
+              fetch("/api/login", {
+                method: "POST",
+                headers: {
+                  "Content-type": "application/json; charset=UTF-8",
+                },
+                body: JSON.stringify({
+                  email: email,
+                  password: password,
+                }),
+              }).then((res) => {
+                if (res.status === 200) {
+                  setLoggedIn(true);
+                  return res.json().then((data) => {
+                    setUser(data);
+                    useStorage().setItem("loggedIn", "true", "session");
+                    useStorage().setItem(
+                      "user",
+                      JSON.stringify(data),
+                      "session"
+                    );
+                  });
+                } else {
+                  res.json().then((data) => console.log(data));
+                }
+              });
+            }}
+          >
+            Sign in
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 };
