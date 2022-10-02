1b90f495608cf2b49c6708c46dea962f393dda76
Dora Qi <doraq@student.ubc.ca>
2022-10-01 22:43:20 -0700

    modification

--- controller/models/interest.ts
+++ controller/models/interest.ts
@@ -1,11 +1,7 @@
 import { interestCollection } from "../db/db";
 
-export const addInterests = async (interest: String): Promise<String | null> => {
-    const result = await interestCollection.find({ name: interest });
-    if (!result.hasNext()) {
-        await interestCollection.insertOne({ name: interest });
-        return interest;
-    } else {
-        return null;
-    }
+// assumes all given interests have not been added
+export const addInterests = async (interests: String[]): Promise<String[]> => {
+        await interestCollection.insertMany(interests.map((str) => {return {name: str}}));
+        return interests;
 };
